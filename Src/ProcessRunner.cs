d0c8c3803365c0f2afe8abbe97584496e61e23ff
rstarkov <sourcecode@roman.st>
2010-04-04 17:48:13 +0100

    RunLogged first check-in (imported from Perforce changelist 1801)

---
+++ Src/ProcessRunner.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using RT.Util;
+using RT.Util.ExtensionMethods;
+
+namespace RunLogged
+{
+    class EventArgs<T> : EventArgs
+    {
+        public T Data { get; private set; }
+        public EventArgs(T data)
+        {
+            Data = data;
+        }
+    }
+
+    class ProcessRunner
+    {
+        private Process _process;
+        private ProcessStartInfo _startInfo;
+        private string _tempStdout, _tempStderr;
+        private Stream _streamStdout, _streamStderr;
+        private PartialUtf8Decoder _utf8Stdout, _utf8Stderr;
+        private Thread _thread;
+        private ThreadExiter _exiter;
+
+        public event EventHandler ProcessExited;
+        public event EventHandler<EventArgs<byte[]>> StdoutData;
+        public event EventHandler<EventArgs<byte[]>> StderrData;
+        public event EventHandler<EventArgs<string>> StdoutText;
+        public event EventHandler<EventArgs<string>> StderrText;
+
+        public int LastExitCode { get; private set; }
+
+        public ProcessRunner(string[] toRun, string workingDir)
+        {
+            _tempStdout = Path.GetTempFileName();
+            _tempStderr = Path.GetTempFileName();
+
+            _startInfo = new ProcessStartInfo();
+            _startInfo.FileName = @"cmd.exe";
+            _startInfo.Arguments = "/C " + escapeCmdline(toRun).JoinString(" ") + @" >{0} 2>{1}".Fmt(_tempStdout, _tempStderr);
+            _startInfo.WorkingDirectory = workingDir;
+            _startInfo.RedirectStandardInput = false;
+            _startInfo.RedirectStandardOutput = false;
+            _startInfo.RedirectStandardError = false;
+            _startInfo.CreateNoWindow = true;
+            _startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            _startInfo.UseShellExecute = false;
+
+            //StartInfo.LoadUserProfile = false;
+            //StartInfo.Domain = "";
+            //StartInfo.UserName = "";
+            //StartInfo.Password = "";
+        }
+
+        private IEnumerable<string> escapeCmdline(IEnumerable<string> args)
+        {
+            foreach (var arg in args)
+            {
+                string newarg = arg.Replace("\\", "\\\\").Replace("\"", "\\\"");
+                if (arg.Contains(" "))
+                    newarg = "\"" + arg + "\"";
+                yield return newarg;
+            }
+        }
+
+        private void thread()
+        {
+            // There's no indication that Process members are thread-safe, so use it on this thread exclusively.
+            _process = new Process();
+            _process.EnableRaisingEvents = true;
+            _process.StartInfo = _startInfo;
+            _process.Start();
+            _utf8Stdout = new PartialUtf8Decoder();
+            _utf8Stderr = new PartialUtf8Decoder();
+            LastExitCode = -1;
+
+            while (!_process.HasExited)
+            {
+                checkOutputs();
+                Thread.Sleep(50);
+            }
+            checkOutputs();
+
+            LastExitCode = _process.ExitCode;
+
+            if (ProcessExited != null)
+                ProcessExited(this, EventArgs.Empty);
+
+            if (_streamStdout != null)
+                _streamStdout.Dispose();
+            if (_streamStderr != null)
+                _streamStderr.Dispose();
+            File.Delete(_tempStdout);
+            File.Delete(_tempStderr);
+            _startInfo = null;
+            _tempStdout = _tempStderr = null;
+            _streamStdout = _streamStderr = null;
+            _utf8Stdout = _utf8Stderr = null;
+            _process = null;
+            _thread = null;
+            _exiter.SignalExited();
+            _exiter = null;
+        }
+
+        private void checkOutputs()
+        {
+            checkOutput(_tempStdout, ref _streamStdout, _utf8Stdout, StdoutData, StdoutText);
+            checkOutput(_tempStderr, ref _streamStderr, _utf8Stderr, StderrData, StderrText);
+        }
+
+        private void checkOutput(string filename, ref Stream stream, PartialUtf8Decoder utf8, EventHandler<EventArgs<byte[]>> dataEvent, EventHandler<EventArgs<string>> textEvent)
+        {
+            if (stream == null)
+            {
+                try { stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite); }
+                catch { }
+            }
+
+            if (stream != null)
+            {
+                var newBytes = stream.ReadAllBytes();
+                if (newBytes.Length > 0)
+                {
+                    if (dataEvent != null)
+                        dataEvent(this, new EventArgs<byte[]>(newBytes));
+                    var newText = utf8.AppendBytes(newBytes);
+                    if (newText.Length > 0 && textEvent != null)
+                        textEvent(this, new EventArgs<string>(newText));
+                }
+            }
+        }
+
+        public void Start()
+        {
+            if (_process != null)
+                throw new InvalidOperationException("Cannot start another instance of the process while the current one is running.");
+
+            _exiter = new ThreadExiter();
+            _thread = new Thread(thread);
+            _thread.Start();
+        }
+
+        public void WaitForExit()
+        {
+            if (_exiter != null)
+                _exiter.WaitExited();
+        }
+    }
+}
---
+++ Src/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using RT.Util;
+using RT.Util.CommandLine;
+using RT.Util.ExtensionMethods;
+
+/// TODO:
+/// - console & no-console variants
+/// - proper (albeit approximate) by-line handling: nice interleaving of stderr with stdout; label the approx time of every line
+/// - send email on failure
+
+namespace RunLogged
+{
+    class Program
+    {
+        public static CmdLineArgs Args;
+        public static StreamWriter Log;
+        public static bool LogIsNew;
+
+        static int Main(string[] args)
+        {
+            WinAPI.SetConsoleOutputCP(65001);
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string originalWorkingDir = Directory.GetCurrentDirectory();
+            try
+            {
+                try
+                {
+                    Args = new CommandLineParser<CmdLineArgs>().Parse(args);
+                }
+                catch (CommandLineParseException e)
+                {
+                    Console.WriteLine();
+                    e.WriteUsageInfoToConsole();
+                    return 1;
+                }
+
+                if (!LogIsNew)
+                    for (int i = 0; i < 3; i++)
+                        outputLine();
+                outputLine("************************************************************************");
+                outputLine("****** RunLogged invoked at {0}".Fmt(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+                outputLine("****** Command: {0}".Fmt(Args.CommandToRun.JoinString("|", "|", " ")));
+                outputLine("****** CurDir: |{0}|".Fmt(Directory.GetCurrentDirectory()));
+                outputLine("****** LogTo: |{0}|".Fmt(Args.Log));
+
+                var runner = new ProcessRunner(Args.CommandToRun, Directory.GetCurrentDirectory());
+                runner.StdoutText += runner_StdoutText;
+                runner.StderrText += runner_StderrText;
+                runner.Start();
+                runner.WaitForExit();
+
+                outputLine("****** completed at {0}".Fmt(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+                outputLine("****** exit code: {0}".Fmt(runner.LastExitCode));
+
+                return runner.LastExitCode;
+            }
+            finally
+            {
+                if (Log != null)
+                    Log.Dispose();
+                try { Directory.SetCurrentDirectory(originalWorkingDir); }
+                catch { } // the script could have theoretically CD'd out of it and deleted it, so don't crash if that happens.
+            }
+        }
+
+        private static void output(string text)
+        {
+            Console.Write(text);
+            Log.Write(text);
+        }
+
+        private static void outputLine()
+        {
+            output(Environment.NewLine);
+        }
+
+        private static void outputLine(string text)
+        {
+            output(text);
+            output(Environment.NewLine);
+        }
+
+        static void runner_StdoutText(object sender, EventArgs<string> e)
+        {
+            output(e.Data);
+        }
+
+        static void runner_StderrText(object sender, EventArgs<string> e)
+        {
+            output("STDERR: ");
+            output(e.Data);
+        }
+    }
+
+    [DocumentationLiteral(@"Runs the specified command and logs all the output to a timestamped log file.")]
+    public class CmdLineArgs : ICommandLineValidatable
+    {
+        [Option("--cd")]
+        [DocumentationLiteral("Optionally changes the working directory while executing the command.")]
+        public string WorkingDir;
+
+        [Option("--log")]
+        [DocumentationLiteral("Log file name pattern. May be relative to --cd WorkingDir. The timestamp YYYY-MM-DD is substituted in place of any occurrences of \"{}\" in the --log parameter.\n\nWhere the --log argument is not specified, the log filename will be constructed from the command to be executed, a timestamp and a .log extension.\n\nIf the log file already exists, it will be appended to. The directory to contain the log file is created automatically if necessary.")]
+        public string Log;
+
+        //email
+
+        [IsPositional]
+        [DocumentationLiteral("Command to be executed, with arguments if any.")]
+        public string[] CommandToRun;
+
+        public string Validate()
+        {
+            if (CommandToRun.Length == 0)
+                return "You must specify the command to be executed (CommandToRun).";
+
+            if (Log == null)
+                Log = Path.GetFileName(CommandToRun[0]).Replace(".", "_") + "--{}.log";
+            Log = Path.GetFullPath(Log.Replace("{}", DateTime.Now.ToString("yyyy-MM-dd")));
+
+            if (WorkingDir != null)
+                try { Directory.SetCurrentDirectory(WorkingDir); }
+                catch { return "Cannot set working directory - check that the directory exists and that the path is valid. \"{0}\"".Fmt(WorkingDir); }
+
+            try
+            {
+                try { Directory.CreateDirectory(Path.GetDirectoryName(Log)); }
+                catch { }
+                Program.LogIsNew = !File.Exists(Log);
+                Program.Log = new StreamWriter(File.Open(Log, FileMode.Append, FileAccess.Write, FileShare.Read));
+                Program.Log.AutoFlush = true;
+            }
+            catch
+            {
+                return "Could not open the log file for writing. File \"{0}\".".Fmt(Log);
+            }
+
+            return null;
+        }
+    }
+}
