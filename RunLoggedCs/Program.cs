e90c89dcc45dca87cec251caa272c4318ceb0a20
Roman Starkov <sourcecode@roman.st>
2025-11-09 00:27:59 +0000

    [Cs] Proof of concept

---
+++ RunLoggedCs/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using RT.Util.ExtensionMethods;
+
+namespace RunLoggedCs;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        var scriptFile = Path.GetFullPath(args[0]);
+
+        using var writer = File.AppendText(args[0] + ".log");
+        Console.SetOut(writer);
+
+        if (!File.Exists(scriptFile))
+        {
+            Console.WriteLine($"ERROR: script file not found: {scriptFile}");
+            return -1;
+        }
+
+        // Load the script
+        var code = File.ReadAllText(scriptFile);
+        code = new[]
+        {
+            "using System;",
+            "using System.Collections.Generic;",
+            "using System.Threading;",
+            "using System.Text.RegularExpressions;",
+            "#line 2",
+            code,
+        }.JoinString("\r\n");
+
+        // Compile the script
+        var tree = CSharpSyntaxTree.ParseText(code, new CSharpParseOptions().WithKind(SourceCodeKind.Regular));
+        var opts = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+        var comp = CSharpCompilation.Create("script", options: opts).AddSyntaxTrees(tree);
+
+        var added = new HashSet<Assembly>();
+        void addAssembly(Assembly assy)
+        {
+            if (!added.Add(assy))
+                return;
+            comp = comp.AddAssemblyReference(assy);
+            foreach (var aref in assy.GetReferencedAssemblies())
+                addAssembly(Assembly.Load(aref));
+        }
+        addAssembly(Assembly.GetEntryAssembly());
+
+        Func<string[], int> main = null;
+        var errors = comp.GetDiagnostics().Where(e => e.Severity == DiagnosticSeverity.Error).ToList();
+        if (errors.Count == 0)
+        {
+            var ms = new MemoryStream();
+            var result = comp.Emit(ms);
+            errors = result.Diagnostics.Where(e => e.Severity == DiagnosticSeverity.Error).ToList();
+            if (result.Success)
+            {
+                var mainCandidates = Assembly.Load(ms.ToArray())
+                    .GetTypes()
+                    .SelectMany(t => t.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
+                    .Where(m => m.Name == "Main")
+                    .ToList();
+                foreach (var candidate in mainCandidates)
+                {
+                    var parameters = candidate.GetParameters();
+                    bool had = main != null;
+                    if (candidate.ReturnType == typeof(void) && parameters.Length == 0)
+                        main = _ => { candidate.Invoke(null, []); return 0; };
+                    else if (candidate.ReturnType == typeof(void) && parameters.Length == 1 && parameters[0].ParameterType == typeof(string[]))
+                        main = args => { candidate.Invoke(null, [args]); return 0; };
+                    else if (candidate.ReturnType == typeof(int) && parameters.Length == 0)
+                        main = _ => (int)candidate.Invoke(null, []);
+                    else if (candidate.ReturnType == typeof(int) && parameters.Length == 1 && parameters[0].ParameterType == typeof(string[]))
+                        main = args => (int)candidate.Invoke(null, [args]);
+                    else
+                        continue;
+                    if (main != null && had)
+                    {
+                        Console.WriteLine($"ERROR: multiple main method candidates");
+                        return -1;
+                    }
+                }
+            }
+        }
+
+        if (main == null)
+        {
+            foreach (var error in errors)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"[{error.Location.GetMappedLineSpan().StartLinePosition}]: {error.GetMessage()}");
+            }
+            return -1;
+        }
+
+        // Execute the script
+        try
+        {
+            return main(args.Skip(1).ToArray());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"UNHANDLED EXCEPTION:");
+            foreach (var excp in e.SelectChain(ee => ee.InnerException))
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{excp.GetType().Name}: {excp.Message}");
+                Console.WriteLine(excp.StackTrace);
+            }
+            Console.WriteLine();
+            return -2;
+        }
+    }
+}
---
+++ RunLoggedCs/SingleFileHelper.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace RunLoggedCs;
+
+public static class SingleFileHelper
+{
+    [SuppressMessage("SingleFile", "IL3000:Avoid accessing Assembly file path when publishing as a single file")]
+    public static CSharpCompilation AddAssemblyReference(this CSharpCompilation ctx, Assembly assy)
+    {
+        if (!string.IsNullOrEmpty(assy.Location))
+            return ctx.AddReferences(MetadataReference.CreateFromFile(assy.Location));
+
+        var entryPath = Process.GetCurrentProcess().MainModule.FileName;
+
+        byte[] bundleSignature =
+        {
+            // 32 bytes represent the bundle signature: SHA-256 for ".net core bundle"
+            0x8b, 0x12, 0x02, 0xb9, 0x6a, 0x61, 0x20, 0x38,
+            0x72, 0x7b, 0x93, 0x02, 0x14, 0xd7, 0xa0, 0x32,
+            0x13, 0xf5, 0xb9, 0xe6, 0xef, 0xae, 0x33, 0x18,
+            0xee, 0x3b, 0x2d, 0xce, 0x24, 0xb3, 0x6a, 0xae
+        };
+
+        using (var memoryMappedPackage = MemoryMappedFile.CreateFromFile(entryPath, FileMode.Open, null, 0, MemoryMappedFileAccess.Read))
+        {
+            using (var packageView = memoryMappedPackage.CreateViewAccessor(0, 0, MemoryMappedFileAccess.Read))
+            {
+                int position = SearchInFile(packageView, bundleSignature);
+                if (position == -1)
+                {
+                    throw new Exception("placeholder not found");
+                }
+
+                long headerOffset = packageView.ReadInt64(position - sizeof(long));
+
+                var manifest = ReadManifest(packageView, headerOffset);
+
+                var entry = manifest.Entries.OrderBy(e => e.RelativePath.Length)
+                    .First(e => e.Type == FileType.Assembly && e.RelativePath.Contains(assy.GetName().Name));
+                var stream = new UnmanagedMemoryStream(packageView.SafeMemoryMappedViewHandle, entry.Offset, entry.Size);
+                return ctx.AddReferences(MetadataReference.CreateFromStream(stream));
+            }
+        }
+    }
+
+    internal static unsafe int SearchInFile(MemoryMappedViewAccessor accessor, byte[] searchPattern)
+    {
+        var safeBuffer = accessor.SafeMemoryMappedViewHandle;
+        return KMPSearch(searchPattern, (byte*)safeBuffer.DangerousGetHandle(), (int)safeBuffer.ByteLength);
+    }
+
+    // See: https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+    private static int[] ComputeKMPFailureFunction(byte[] pattern)
+    {
+        int[] table = new int[pattern.Length];
+        if (pattern.Length >= 1)
+        {
+            table[0] = -1;
+        }
+
+        if (pattern.Length >= 2)
+        {
+            table[1] = 0;
+        }
+
+        int pos = 2;
+        int cnd = 0;
+        while (pos < pattern.Length)
+        {
+            if (pattern[pos - 1] == pattern[cnd])
+            {
+                table[pos] = cnd + 1;
+                cnd++;
+                pos++;
+            }
+            else if (cnd > 0)
+            {
+                cnd = table[cnd];
+            }
+            else
+            {
+                table[pos] = 0;
+                pos++;
+            }
+        }
+
+        return table;
+    }
+
+    // See: https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+    private static unsafe int KMPSearch(byte[] pattern, byte* bytes, long bytesLength)
+    {
+        int m = 0;
+        int i = 0;
+        int[] table = ComputeKMPFailureFunction(pattern);
+
+        while (m + i < bytesLength)
+        {
+            if (pattern[i] == bytes[m + i])
+            {
+                if (i == pattern.Length - 1)
+                {
+                    return m;
+                }
+
+                i++;
+            }
+            else
+            {
+                if (table[i] > -1)
+                {
+                    m = m + i - table[i];
+                    i = table[i];
+                }
+                else
+                {
+                    m++;
+                    i = 0;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public struct Header
+    {
+        public uint MajorVersion;
+        public uint MinorVersion;
+        public int FileCount;
+        public string BundleID;
+
+        // Fields introduced with v2:
+        public long DepsJsonOffset;
+        public long DepsJsonSize;
+        public long RuntimeConfigJsonOffset;
+        public long RuntimeConfigJsonSize;
+        public ulong Flags;
+
+        public ImmutableArray<Entry> Entries;
+    }
+
+    /// <summary>
+    /// FileType: Identifies the type of file embedded into the bundle.
+    ///
+    /// The bundler differentiates a few kinds of files via the manifest,
+    /// with respect to the way in which they'll be used by the runtime.
+    /// </summary>
+    public enum FileType : byte
+    {
+        Unknown, // Type not determined.
+        Assembly, // IL and R2R Assemblies
+        NativeBinary, // NativeBinaries
+        DepsJson, // .deps.json configuration file
+        RuntimeConfigJson, // .runtimeconfig.json configuration file
+        Symbols // PDB Files
+    };
+
+    public struct Entry
+    {
+        public long Offset;
+        public long Size;
+        public long CompressedSize; // 0 if not compressed, otherwise the compressed size in the bundle
+        public FileType Type;
+        public string RelativePath; // Path of an embedded file, relative to the Bundle source-directory.
+    }
+
+    static UnmanagedMemoryStream AsStream(MemoryMappedViewAccessor view)
+    {
+        long size = checked((long)view.SafeMemoryMappedViewHandle.ByteLength);
+        return new UnmanagedMemoryStream(view.SafeMemoryMappedViewHandle, 0, size);
+    }
+
+    public static Header ReadManifest(MemoryMappedViewAccessor view, long bundleHeaderOffset)
+    {
+        using var stream = AsStream(view);
+        stream.Seek(bundleHeaderOffset, SeekOrigin.Begin);
+        return ReadManifest(stream);
+    }
+
+    public static Header ReadManifest(Stream stream)
+    {
+        var header = new Header();
+        using var reader = new BinaryReader(stream, Encoding.UTF8, leaveOpen: true);
+        header.MajorVersion = reader.ReadUInt32();
+        header.MinorVersion = reader.ReadUInt32();
+
+        // Major versions 3, 4 and 5 were skipped to align bundle versioning with .NET versioning scheme
+        if (header.MajorVersion < 1 || header.MajorVersion > 6)
+        {
+            throw new InvalidDataException($"Unsupported manifest version: {header.MajorVersion}.{header.MinorVersion}");
+        }
+
+        header.FileCount = reader.ReadInt32();
+        header.BundleID = reader.ReadString();
+        if (header.MajorVersion >= 2)
+        {
+            header.DepsJsonOffset = reader.ReadInt64();
+            header.DepsJsonSize = reader.ReadInt64();
+            header.RuntimeConfigJsonOffset = reader.ReadInt64();
+            header.RuntimeConfigJsonSize = reader.ReadInt64();
+            header.Flags = reader.ReadUInt64();
+        }
+
+        var entries = ImmutableArray.CreateBuilder<Entry>(header.FileCount);
+        for (int i = 0; i < header.FileCount; i++)
+        {
+            entries.Add(ReadEntry(reader, header.MajorVersion));
+        }
+
+        header.Entries = entries.MoveToImmutable();
+        return header;
+    }
+
+    private static Entry ReadEntry(BinaryReader reader, uint bundleMajorVersion)
+    {
+        Entry entry;
+        entry.Offset = reader.ReadInt64();
+        entry.Size = reader.ReadInt64();
+        entry.CompressedSize = bundleMajorVersion >= 6 ? reader.ReadInt64() : 0;
+        entry.Type = (FileType)reader.ReadByte();
+        entry.RelativePath = reader.ReadString();
+        return entry;
+    }
+}
